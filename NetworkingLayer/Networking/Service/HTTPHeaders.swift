fa31c181bee5eeadbab1c23325deb321b4c52db9
Mateusz Bąk <mateusz.bak@x-kom.pl>
2020-07-17 08:13:57 +0200

    Add base project

---
+++ NetworkingLayer/Networking/Service/HTTPHeaders.swift
@@ -0,0 +1,11 @@
+//
+//  HTTPHeaders.swift
+//  NetworkingLayer
+//
+//  Created by x-kom Mateusz Bąk on 12/05/2020.
+//  Copyright © 2020 x-kom. All rights reserved.
+//
+
+import Foundation
+
+public typealias HTTPHeaders = [String: String]
---
+++ NetworkingLayer/Networking/Service/HTTPMethod.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPMethod.swift
+//  NetworkingLayer
+//
+//  Created by x-kom Mateusz Bąk on 12/05/2020.
+//  Copyright © 2020 x-kom. All rights reserved.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+	case get = "GET"
+	case post = "POST"
+	case put = "PUT"
+	case patch = "PATCH"
+	case delete = "DELETE"
+}
